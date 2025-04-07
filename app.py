f4106f153193b9438998dbdec112ab41fa073f46
elijahsandler <sandler.e@northeastern.edu>
2025-04-07 11:34:12 -0400

    AWS S3 file upload with Streamlit interface

---
+++ app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+from helper import load_env_variables, upload_to_s3
+import boto3
+
+# load aws credentials
+aws_credentials = load_env_variables()
+
+# verify credentials
+if not aws_credentials["aws_access_key_id"]:
+    raise ValueError("No AWS Access key id set")
+if not aws_credentials["aws_secret_access_key"]:
+    raise ValueError("No AWS Secret Access key set")
+if not aws_credentials["aws_region"]:
+    raise ValueError("No AWS Region Set")
+if not aws_credentials["s3_bucket_name"]:
+    raise ValueError("S3_BUCKET_NAME environment variable is not set")
+
+# initialize client
+s3_client = boto3.client(
+        "s3",
+        aws_access_key_id=aws_credentials["aws_access_key_id"],
+        aws_secret_access_key=aws_credentials["aws_secret_access_key"],
+        region_name=aws_credentials["aws_region"],
+    )
+
+# upload files via streamlit
+uploads = st.file_uploader(
+    "Choose files to upload:", accept_multiple_files=True
+)
+
+# upload
+for file in uploads:
+    st.write("Processing", file.name)
+    e = upload_to_s3(s3_client, file, aws_credentials["s3_bucket_name"])
+    st.write(e)
+
---
+++ helper/helper.py
@@ -0,0 +1,23 @@
+# Example script to simulate a user uploading a JSON file every
+# 3 seconds to an S3 bucket.
+
+import os
+from dotenv import load_dotenv
+
+# Load the values from .env into dictionary
+def load_env_variables():
+    load_dotenv()
+    return {
+        "aws_access_key_id": os.getenv("AWS_ACCESS_KEY_ID"),
+        "aws_secret_access_key": os.getenv("AWS_SECRET_ACCESS_KEY"),
+        "aws_region": os.getenv("AWS_REGION", "us-east-1"),
+        "s3_bucket_name": os.getenv("S3_BUCKET_NAME"),
+    }
+
+
+def upload_to_s3(s3_client, file, bucket_name):
+    try:
+        s3_client.upload_fileobj(file, bucket_name, f"uploads/{file.name}")
+        return f'{file.name} uploaded successfully'
+    except Exception as e:
+        return f'an error occured while uploading {file.name}: {e}'
