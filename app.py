6e9271f99732ff79e3974e3ace2c0e133ea43b0b
elijahsandler <sandler.e@northeastern.edu>
2025-04-07 18:09:14 -0400

    added tabs

--- app.py
+++ app.py
@@ -1,38 +1,31 @@
 import streamlit as st
-from helper import load_env_variables, upload_to_s3
+from helper import upload_to_s3
 import boto3
 
 BUCKET_NAME = "ds4300-jamsters-project"
 
-# # load aws credentials
-# aws_credentials = load_env_variables()
-
-# # verify credentials
-# if not aws_credentials["aws_access_key_id"]:
-#     raise ValueError("No AWS Access key id set")
-# if not aws_credentials["aws_secret_access_key"]:
-#     raise ValueError("No AWS Secret Access key set")
-# if not aws_credentials["aws_region"]:
-#     raise ValueError("No AWS Region Set")
-# if not aws_credentials["s3_bucket_name"]:
-#     raise ValueError("S3_BUCKET_NAME environment variable is not set")
-
 # initialize client
 s3_client = boto3.client(
         "s3",
-        # aws_access_key_id=aws_credentials["aws_access_key_id"],
-        # aws_secret_access_key=aws_credentials["aws_secret_access_key"],
         region_name='us-east-2',
     )
 
-# upload files via streamlit
-uploads = st.file_uploader(
-    "Choose files to upload:", accept_multiple_files=True
-)
+tab1, tab2 = st.tabs(["S3 File Upload", "RDS Data Visualization"])
 
-# upload
-for file in uploads:
-    st.write("Processing", str(file.name) + '...')
-    e = upload_to_s3(s3_client, file, BUCKET_NAME)
-    st.write(e)
+with tab1: 
+    st.header("Upload files to S3")
+
+    # upload files via streamlit
+    uploads = st.file_uploader(
+        "Choose files to upload:", accept_multiple_files=True
+    )
+
+    # upload
+    for file in uploads:
+        st.write("Processing", str(file.name) + '...')
+        e = upload_to_s3(s3_client, file, BUCKET_NAME)
+        st.write(e)
+
+with tab2: 
+    st.header("Data Visualization from RDS")
 
